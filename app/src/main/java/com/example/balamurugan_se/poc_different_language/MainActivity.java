4c2f7b36548ca13b76ed795b8c93ab4e87a287a5
balabhuvana <balan.ssn@gmail.com>
2016-08-10 13:11:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/balamurugan_se/poc_different_language/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.balamurugan_se.poc_different_language;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
